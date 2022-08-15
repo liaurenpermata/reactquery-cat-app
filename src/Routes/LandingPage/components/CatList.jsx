417534d5ed043b683ea0060934bd9fda8eaf6feb
Liauren Permata Sari <liaurenpermata@gmail.com>
2022-08-15 12:19:29 +0700

    add data fetching

--- src/Routes/LandingPage/components/CatList.jsx
+++ src/Routes/LandingPage/components/CatList.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useQuery } from 'react-query';
 
 import getCats from '../../../Services/fetchApi';
@@ -6,10 +6,33 @@ import LoadingState from './LoadingState';
 import './catList.css';
 
 const View = () => {
+  const [selectedCatIdx, setSelectedCatIdx] = useState(null);
   // useQuery here
+  const { data: catListData, isLoading } = useQuery('catList', getCats)
 
   // Loading state here
+  if(isLoading){
+    return <div>loading...</div>;
+  }
 
-  return <div className="padding">Hello Felines!</div>;
+  return (
+    <div className="padding">
+      <h1>List Cat</h1>
+      <div className='name-list'>
+        {
+          catListData.map((cat, idx) => (
+            <li className='list-cat' key={idx} onClick={() => {setSelectedCatIdx(idx)}}>
+              {cat.name}
+            </li>
+          ))
+        }
+      </div>
+      {
+        selectedCatIdx!==null && (
+          <img src={catListData[selectedCatIdx].image.url} alt={catListData[selectedCatIdx].name}/>
+        )
+      }
+    </div>
+  )
 };
 export default View;
